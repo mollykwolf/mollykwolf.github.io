f6231e1e729741283b7124e81f1725265f63d4e4
Molly Wolf <119366082+mollykwolf@users.noreply.github.com>
2023-05-13 11:10:46 -0700

    started stlying project tiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,10 @@
 import React from "react"
-import LinkedInIcon from "@material-ui/icons/LinkedIn"
-import GitHubIcon from "@material-ui/icons/GitHub"
 import "../styles/Footer.css"
-
 
 const Footer = () => {
   return( 
     <div className="footer">
-      {/* <div className="socialMedia"> */}
-        {/* <LinkedInIcon />
-        <GitHubIcon /> */}
-      {/* </div> */}
-      <p> &copy; Molly Wolf | 2023 | <GitHubIcon /> |   <LinkedInIcon /> </p>
+      <p> &copy; Molly Wolf | 2023 </p>
     </div>
   )
 }
